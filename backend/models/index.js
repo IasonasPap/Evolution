a3f45553ea952e895f9519a3eb748b0defbc92b6
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2020-12-30 19:28:32 +0200

    Complete database creation
    Create foreign key constraints between entities and export the whole database object to be used by other files. Finalize database creation.

--- backend/models/index.js
+++ backend/models/index.js
@@ -18,5 +18,45 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+user = require("./user.model.js")(sequelize, Sequelize);
+charger = require("./charger.model.js")(sequelize, Sequelize);
+electricVehicle = require("./electricVehicle.model.js")(sequelize, Sequelize);
+station = require("./station.model.js")(sequelize, Sequelize);
+chargingSession = require("./chargingSession.model.js")(sequelize, Sequelize);
+chargingPoint = require("./chargingPoint.model.js")(sequelize, Sequelize);
+energyProvider = require("./energyProvider.model.js")(sequelize, Sequelize);
+
+user.hasMany(electricVehicle);
+electricVehicle.belongsTo(user);
+
+user.hasMany(station);
+station.belongsTo(user);
+
+charger.hasMany(electricVehicle);
+electricVehicle.belongsTo(charger);
+
+charger.hasMany(chargingPoint);
+chargingPoint.belongsTo(charger);
+
+station.hasMany(chargingPoint);
+chargingPoint.belongsTo(station);
+
+electricVehicle.hasMany(chargingSession);
+chargingSession.belongsTo(electricVehicle);
+
+chargingPoint.hasMany(chargingSession);
+chargingSession.belongsTo(chargingPoint);
+
+energyProvider.hasMany(station);
+station.belongsTo(energyProvider);
+
+db.user = user;
+db.charger = charger;
+db.electricVehicle = electricVehicle;
+db.station = station;
+db.chargingSession = chargingSession;
+db.chargingPoint = chargingPoint;
+db.energyProvider = energyProvider;
+
 module.exports = db;
 
