44aa3d4c9d389fb6ed4e787f79f3a4a38a6c0051
aris <arisprv@spectre.gr>
2021-01-02 20:05:53 +0200

    small fix

--- backend/routes/chargingSession.routes.js
+++ backend/routes/chargingSession.routes.js
@@ -7,6 +7,7 @@ var router = require("express").Router();
 router.post("/", chargingSession.create);
 
 //retrieve the charging Session of a single point in a period of time
+
 router.get("/SessionsPerPoint/:pointId/:datetimeFrom/:datetimeTo", auth, chargingSession.findAll);
 router.get("/SessionsPerStation/:stationId/:datetimeFrom/:datetimeTo", auth, chargingSession.findAll);
 router.get("/SessionsPerEV/:vehicleId/:datetimeFrom/:datetimeTo", auth, chargingSession.findAll);
