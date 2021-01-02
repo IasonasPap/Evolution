3290ad2f2aa12416606d12cce136cf6f979e5e52
aris <arisprv@spectre.gr>
2021-01-02 02:15:04 +0200

    add login logout and auth-middleware

--- backend/routes/chargingSession.routes.js
+++ backend/routes/chargingSession.routes.js
@@ -1,4 +1,5 @@
 const chargingSession = require('../controllers/chargingSession.controller.js');
+const auth = require('../middleware/auth');
 
 var router = require("express").Router();
 
@@ -6,7 +7,7 @@ var router = require("express").Router();
 router.post("/", chargingSession.create);
 
 //retrieve the charging Session of a sigle point in a period of time
-router.get("/SessionsPerPoint/:pointId/:datetimeFrom/:datetimeTo", chargingSession.findAll);
+router.get("/SessionsPerPoint/:pointId/:datetimeFrom/:datetimeTo", auth, chargingSession.findAll);
 router.get("/SessionsPerStation/:stationId/:datetimeFrom/:datetimeTo", chargingSession.findAll);
 router.get("/SessionsPerEV/:vehicleId/:datetimeFrom/:datetimeTo", chargingSession.findAll);
 router.get("/SessionsPerProvider/:providerId/:datetimeFrom/:datetimeTo", chargingSession.findAll);
--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const userRoutes = require('./user.routes');
 const adminRoutes = require('./admin.routes');
 const chargingSessionRoutes = require('./chargingSession.routes');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/admin', adminRoutes);
 router.use('/', chargingSessionRoutes);
+
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
