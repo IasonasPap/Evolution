0605da3fefd9ccba9443a9d8eeeb45b7b8bed89c
FayStatha <fay.statha@gmail.com>
2021-01-09 10:09:57 +0200

    Debug resetsessions and small fixes

--- cli/resetsessions.js
+++ cli/resetsessions.js
@@ -2,35 +2,35 @@ const db = require('../backend/models');
 const {user, chargingSession, sequelize} = db;
 
 module.exports = () => {
-    try {
-        const [_, newlyCreated] = user.findOrCreate({
-            where: {
-                username: "admin"
-            },
-            defaults: {
-                username: "admin",
-                password: "petrol4ever",
-                fullName: "test",
-                email: "test@evolution.com",
-                isAdmin: 1,
-                isStationManager: 0
-            }
-        });
-        console.log({message: `Admin user was ${newlyCreated?'created':'reset'}!`});
-    }
-    catch (err) {
-        console.log({error: "Something went wrong!"});
-    }
 
-    // Reset chanrgingSession table, that is, delete every row in the entity
-    chargingSession.destroy({
-        truncate: true
-    }).then(() => {
-        console.log({message: "ChargingSessions table was reset!"});
-    }).catch((err) => {
-        console.log({error: "Something went wrong!"});
-    })
-    .then(() => {
+    return user.upsert({
+        username: 'admin',
+        password: 'petrol4ever',
+        email: "defaultadmin@evolution.com",
+        fullName: "Default Admin",
+        isAdmin: 1,
+        isStationOwner: 0
+    }).then(([user, isNew]) => {
+        if (isNew){
+            console.log({message: "Default admin was created successfully!"})
+        }
+        else {
+            console.log({message: "Default admin was updated successfully!"})
+        }
+    }).catch(() =>{
+        console.log({error: "Something went wrong with update/creation of default admin!"})
         process.exit()
+    }).then(() => {
+        // Reset chanrgingSession table, that is, delete every row in the entity
+        chargingSession.destroy({
+            truncate: true
+        }).then(() => {
+            console.log({message: "ChargingSessions table was reset!"});
+        }).catch((err) => {
+            console.log({error: "Something went wrong!"});
+        })
+        .then(() => {
+            process.exit()
+        })
     })
 }
