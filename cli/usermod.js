ec2e8d884875527ab57efc46ac02d9ab5888cce3
FayStatha <fay.statha@gmail.com>
2021-01-09 10:52:40 +0200

    Insert or update user

--- cli/usermod.js
+++ cli/usermod.js
@@ -2,24 +2,37 @@ const db = require('../backend/models');
 const {user, chargingSession, sequelize} = db;
 const bcrypt = require('bcrypt')
 
-module.exports = (username, password) => {
-    // needs changes with other columns
-    passw = bcrypt.hashSync(password, 8)
-    return user.upsert({
-        username: username,
-        password: passw,
-        email: "skatoules@gmail.com",
-        fullName: "Papitso Pasit",
-        isAdmin: 0,
-        isStationOwner: 0
-    }).then(([user, isNew]) => {
-        if (isNew){
-            console.log({message: "User was created successfully!"})
+module.exports = (username, password, fullName, email, isAdmin, isStationManager) => {
+
+    return user.findOne({where: {username: username}}).then(my_user => {
+
+        if (!my_user){
+            return user.create({
+                username: username,
+                password: password,
+                fullName: fullName,
+                email: email,
+                isAdmin: isAdmin,
+                isStationManager: isStationManager
+            }).then((_) =>{
+                return false
+            })
         }
         else {
-            console.log({message: "User was updated successfully!"})
+            return my_user.update({
+                password: password
+            }).then((_) => {
+                return true
+            })
         }
-    }).catch(() =>{
+    }).then((isNew) => {
+        if(!isNew){
+            console.log({message: "User created successfully!"})
+        }
+        else {
+            console.log({message: "User updated successfully!"})
+        }
+    }).catch((err) => {
         console.log({error: "Something went wrong with update/creation of user!"})
         process.exit()
     })
