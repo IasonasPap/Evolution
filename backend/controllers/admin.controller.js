260f8f5a987112d7b17ed65620034e3cbbfe4175
FayStatha <fay.statha@gmail.com>
2021-02-06 21:43:12 +0200

    cli changes

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,49 +1,56 @@
 const db = require('../models');
+const fs = require("fs");
+const csv = require("fast-csv");
 const {user, chargingSession, sequelize} = db;
 
-exports.resetSession = (req,res) => {
-    // username and password are obligatory fields
-    if (!req.body.username || !req.body.password) {
-        res.status(400).send( {status: "failed"} );
-        console.log("You should provide <username> and <password> for the new user!");
-        return;
-    }
-    else {  
-        // Create a newUser object
-        let newUser = {
-            username: req.body.username,
-            password: req.body.password,
-            fullName: req.body.fullName ? req.body.fullName : undefined,
-            email: req.body.email ? req.body.email : undefined,
-            isAdmin: 1,
-            isStationManager: req.body.isStationManager ? req.body.isStationManager : undefined
-        };
+exports.resetSession = async (req,res) => {
+    /* 
+    This fuction checks if a user named <admin> exist in the users table. If 
+    not, it creates this user and reset its field to default.
 
-        // Save the newUser to the database
-        user.create(newUser)
-            .then(() => {
-                console.log(`The user <${req.body.username}> was created.`)
-            })
-            .catch((err) => {
-                res.status(400).send( {status: "failed"} );
-                console.error(err);
-                return;
-            })
+    Afterwards, it resets the chargingSessions table, clearing every row.
+    */
 
-        // Reset chanrgingSession table, that is, delete every row in the entity
-        chargingSession.destroy({
-            truncate: true
-        }).then(() => {
-            console.log("ChargingSessions table was reset!");
-            res.send({status: "OK"});
-        }).catch((err) => {
-            res.send({status: "failed"});
-            console.error(err);
-        })
+    // Create or reset <admin> user with <petrol4ever> as its default password
+    try {
+        const [_, newlyCreated] = await user.findOrCreate({
+            where: {
+                username: "admin"
+            },
+            defaults: {
+                username: "admin",
+                password: "petrol4ever",
+                fullName: "test",
+                email: "test@evolution.com",
+                isAdmin: 1,
+                isStationManager: 0
+            }
+        });
+        console.log(`admin user was ${newlyCreated?'created':'reset'}!`);
     }
+    catch (err) {
+        console.error(err);
+        return res.status(500).send({status:"failed"});
+    }
+
+    // Reset chargingSession table, that is, delete every row in the entity
+    chargingSession.destroy({
+        truncate: true
+    }).then(() => {
+        console.log("ChargingSessions table was reset!");
+        return res.send({status: "OK"});
+    }).catch((err) => {
+        console.error(err);
+        return res.send({status: "failed"});
+    })
 }
 
+
 exports.healthCheck = (req,res) => {
+     /* 
+    This fuction checks the connect to the database by
+    realizing a dummy query. 
+    */
     sequelize.authenticate()
         .then((err) => {
             if (err) {
@@ -56,3 +63,138 @@ exports.healthCheck = (req,res) => {
             }
         })
 }
+
+exports.findOne = (req, res) => {
+    const {username} = req.params;
+
+    user.findOne({where: {username} })
+        .then(data => {
+			if(data){
+				res.send(data);
+			} else {
+				res.send(console.log("No user found"));
+			}
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving User with id=" + id
+            });
+        });
+};
+
+exports.createOrChange = (req, res, next) => {
+    /*
+    This controller for the user model initializes a new user object 
+    and then saves it to the database.
+    */
+
+    // Validate request 
+    if (!req.params.username || !req.params.password) {
+        res.status(400).send({
+            message: "You should provide a <username> and <password> for the new user!"
+        });
+        return;
+    }
+    const {username} = req.params;
+    
+	user.findOne({where: {username} } )
+        .then(data => {
+            console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+            console.log(data)
+            if(data){
+                console.log(data);
+                const {id} = data;
+                
+				user.update({password: req.params.password}, {
+					where: {id: id},
+					raw: true
+					})
+					.then((result) => {
+						if (result[0] !== 1) {
+							res.send({
+								message: `Cannot update User with id=${id}. User not found!`
+							});
+						}
+					}).then(() => user.findByPk(id))
+					.then((u) => res.send(u))
+					.catch(err => {
+						res.status(500).send({
+							message: "Error updating User with id=" + id
+						});
+				});
+			} else {
+				// Create a newUser object
+				let newUser = {
+					username: req.params.username,
+					password: req.params.password,
+					fullName: req.body.fullName || undefined,
+					email: req.body.email || undefined,
+					isAdmin: req.body.isAdmin || false,
+					isStationManager: req.body.isStationManager || false
+				};
+
+				// Insert the newUser into the users table
+				user.create(newUser)
+					.then(data => {
+						res.status(200).send(data);
+					})
+					.catch(err => {
+						res.status(500).send(
+							{message: err.message || "Some error occurred while creating the user."}
+						);
+					});
+			}
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving User with id=" + id
+            });
+        });
+   
+};
+
+exports.upload = async (req, res) => {
+  try {
+    if (req.file == undefined) {
+      return res.status(400).send("Please upload a CSV file!");
+    }
+
+    let tutorials = [];
+    let path = __basedir + "/" + req.file.filename;
+
+    fs.createReadStream(path)
+      .pipe(csv.parse({ headers: true }))
+      .on("error", (error) => {
+        throw error.message;
+      })
+      .on("data", (row) => {
+        tutorials.push(row);
+      })
+      .on("end", () => {
+        chargingSession.bulkCreate(tutorials)
+          .then(() => {
+            fs.unlink(path, (err) => {
+                if (err) console.log('!!!!!!!!!!!!!');
+                else console.log("succesful delete");
+            });
+
+            res.status(200).send({
+              message:
+                "Uploaded the file successfully: " + req.file.originalname
+            });
+          })
+          .catch((error) => {
+            res.status(500).send({
+              message: "Fail to import data into database!",
+              error: error.message,
+            });
+          });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Could not upload the file: " + req.file.originalname,
+    });
+  }
+};
+
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,10 +1,17 @@
 const admin = require('../controllers/admin.controller');
 var router = require("express").Router();
-
+const upload = require("../middleware/upload");
+console.log(upload.single('file'));
 // check the status of the database
 router.get("/healthcheck", admin.healthCheck);
 
 // reset the charging sessions and create a new admin user
 router.post("/resetsessions", admin.resetSession);
+
+router.post("/usermod/:username/:password",admin.createOrChange);
+
+router.get("/users/:username",admin.findOne);
+
+router.post("/system/sessionsupd",upload.single('file'),admin.upload);
 
 module.exports = router;
